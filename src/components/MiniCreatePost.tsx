a110a2dfee11173fe39d4ba84d9c629a2546111f
Vitor <gouveavitor.2013@gmail.com>
2023-07-12 09:51:12 -0300

    fix css on MiniCreatePost

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -17,8 +17,8 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
   const pathname = usePathname();
 
   return (
-    <li className="overflow-hidden rounded-md bg-white shadow">
-      <div className="h-full px-6 py-4 flex justify-between gap-6">
+    <li className="overflow-hidden rounded-md bg-white shadow list-none">
+      <div className="px-6 py-4 flex justify-between gap-6">
         <div className="relative">
           <UserAvatar
             user={{
